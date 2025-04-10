e28a7b2ea6e84a37e8cdbee5588e19e6867efec6
Ibrahima Niang <papeibaniang@yahoo.fr>
2025-04-10 11:17:49 -0400

    affichage des boutons de filtre par catégorie

--- FrontEnd/js/travaux.js
+++ FrontEnd/js/travaux.js
@@ -9,11 +9,29 @@ async function ajoutDesTravaux(){
         figcaption.textContent = travaux[i].title;
         img.src = travaux[i].imageUrl;
         img.alt = travaux[i].title;
+        figure.dataset.idTravaux = travaux[i].id;
+        figure.dataset.categoryId = travaux[i].categoryId;
+        figure.dataset.userId = travaux[i].userId;
         figure.appendChild(img);
         figure.appendChild(figcaption);
         gallery.appendChild(figure);
     }
 }
-window.onload = function(){
-    ajoutDesTravaux();
-};
+async function ajoutCategories() {
+    const reponse = await fetch("http://localhost:5678/api/categories")
+    const categories = await reponse.json();
+    const divBouton = document.getElementsByClassName("filtre")[0]
+    let boutonTous = document.createElement("button");
+    boutonTous.textContent = "Tous";
+    //Pour le bouton tous on met un id de 0 
+    boutonTous.dataset.idCategorie = 0;
+    divBouton.appendChild(boutonTous);
+    for (let i = 0; i < categories.length; i++) {
+        let bouton = document.createElement("button")
+        bouton.textContent = categories[i].name;
+        bouton.dataset.idCategorie = categories[i].id;
+        divBouton.appendChild(bouton);        
+    }
+}
+ajoutDesTravaux();
+ajoutCategories();
